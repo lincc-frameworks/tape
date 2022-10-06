45ee602dd04cc93920d8f4e0ac00f66ad84f21d7
Doug Branton <brantd@uw.edu>
2022-10-06 16:14:56 -0400

    initial dataframe, package setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='lsstseries',
+    version='0.1.0',    
+    description='',
+    url='https://github.com/lincc-frameworks/lsstseries',
+    license='BSD 2-clause',
+    packages=['lsstseries'],
+    install_requires=['pandas',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
