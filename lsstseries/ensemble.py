58ec58801c74efefbc70278e4e512b085ab82fa7
nevencaplar <ncaplar@princeton.edu>
2022-10-20 16:13:00 -0700

    Update with flake8 check & docstrings modifications
    Updated docstrings to be consistent with LSST practices. I have not modified @property, private functions or docstrings for classes. Also, I modified the code to be consistent with LSST view of PEP8

--- lsstseries/ensemble.py
+++ lsstseries/ensemble.py
@@ -1,41 +1,80 @@
 import pandas as pd
-import numpy as np
 import pyvo as vo
 from .timeseries import timeseries
 
+
 class ensemble():
     """ensemble object is a collection of light curve ids"""
-    def __init__(self,token=None):
-        self.result = None #holds the latest query
+    def __init__(self, token=None):
+        self.result = None  # holds the latest query
         self.token = token
 
     def tap_token(self, token):
         """Add/update a TAP token to the class, enables querying
         Read here for information on TAP access: https://data.lsst.cloud/api-aspect
-        """
-        self.token=token
 
-    def query_tap(self,query,maxrec=None):
-        """query the TAP service, query is an ADQL formatted string"""
+        Parameters
+        ----------
+        token : `str`
+            Token string
+        """
+        self.token = token
+
+    def query_tap(self, query, maxrec=None):
+        """Query the TAP service
+
+        Parameters
+        ----------
+        query : `str`
+            Query is an ADQL formatted string
+        maxrec: `int`
+            Max number of results returned
+
+        Returns
+        ----------
+        result: `pd.df`
+            Result of the query, as pandas dataframe
+        """
         cred = vo.auth.CredentialStore()
         cred.set_password("x-oauth-basic", self.token)
 
-        service = vo.dal.TAPService("https://data.lsst.cloud/api/tap",cred.get("ivo://ivoa.net/sso#BasicAA"))
+        service = vo.dal.TAPService("https://data.lsst.cloud/api/tap", cred.get("ivo://ivoa.net/sso#BasicAA"))
         results = service.search(query, maxrec=maxrec)
         result = results.to_table().to_pandas()
         self.result = result
         return result
 
-    def query_ids(self, ids, 
-                  core_cols=['midPointTai','psFlux','psFluxErr'],
+    def query_ids(self, ids,
+                  core_cols=['midPointTai', 'psFlux', 'psFluxErr'],
                   add_cols=[],
                   id_field='diaObjectId',
                   catalog='dp02_dc2_catalogs',
                   table='DiaSource',
                   maxrec=None):
-        """query based on a list of object ids"""
+        """Query based on a list of object ids; applicable for DP0.2
+
+        Parameters
+        ----------
+        ids: `int`
+            Ids of object
+        core_cols: `list` of `str`
+            Time, flux and flux error columns
+        add_cols: `list` of `str`
+            Additional colums to retreive
+        id_field: `str`
+            Which Id is being queried
+        catalog: `str`
+            Source catalog
+        table: `str`
+            Source table
+
+        Returns
+        ----------
+        result: `pd.df`
+            Result of the query, as pandas dataframe
+        """
         cols = core_cols+add_cols
-        idx_cols = ['diaObjectId','filterName']
+        idx_cols = ['diaObjectId', 'filterName']
 
         result = pd.DataFrame(columns=idx_cols+cols)
         select_cols = ",".join(idx_cols)+','+','.join(cols)
@@ -43,28 +82,40 @@ class ensemble():
         id_list = "("+",".join(str_ids)+")"
 
         result = self.query_tap(f"SELECT {select_cols} "
-                            f"FROM {catalog}.{table} "
-                            f"WHERE {id_field} IN {id_list}")
-        index = self._build_index(result['diaObjectId'],result['filterName'])
+                                f"FROM {catalog}.{table} "
+                                f"WHERE {id_field} IN {id_list}")
+        index = self._build_index(result['diaObjectId'], result['filterName'])
         result.index = index
         result = result[cols].sort_index()
         self.result = result
         return result
 
-    def to_timeseries(self,dataframe,target,core_cols=['midPointTai','psFlux','psFluxErr'],add_cols=[]):
-        """construct a timeseries object from one target object_id, assumes that the result
-        is a collection of lightcurves (output from query_ids)"""
-        cols = core_cols+add_cols
+    def to_timeseries(self, dataframe, target, core_cols=['midPointTai', 'psFlux', 'psFluxErr'], add_cols=[]):
+        """Construct a timeseries object from one target object_id, assumes that the result
+        is a collection of lightcurves (output from query_ids)
+
+        Parameters
+        ----------
+        dataframe: `pd.df`
+            Ensamble object
+        target: `int`
+            Id of a source to be extracted
+
+        Returns
+        ----------
+        ts: `timeseries`
+            Timeseries for a single object
+        """
+        # cols = core_cols + add_cols
         df = dataframe.xs(target)
-        ts = timeseries()._from_ensemble(data=df,object_id=target)
+        ts = timeseries()._from_ensemble(data=df, object_id=target)
         return ts
 
-    def _build_index(self,obj_id, band):
+    def _build_index(self, obj_id, band):
         """Build pandas multiindex from object_ids and bands"""
-        #Create a multiindex
+        # Create a multiindex
 
-        idx = range(len(list(zip(obj_id,band))))
-        tuples = zip(obj_id,band,idx)
-        index = pd.MultiIndex.from_tuples(tuples, names=["object_id","band", "index"])
+        idx = range(len(list(zip(obj_id, band))))
+        tuples = zip(obj_id, band, idx)
+        index = pd.MultiIndex.from_tuples(tuples, names=["object_id", "band", "index"])
         return index
-
--- lsstseries/timeseries.py
+++ lsstseries/timeseries.py
@@ -1,50 +1,58 @@
 import pandas as pd
-import numpy as np
+
 
 class timeseries():
     def __init__(self):
         self.data = None
         self.id = None
-        self.cols = ['time','flux','flux_err']
-    
-    #I/O
-    def from_dict(self,data_dict,band_label='band'):
-        """Build dataframe from a python dictionary"""
+        self.cols = ['time', 'flux', 'flux_err']
+
+    # I/O
+    def from_dict(self, data_dict, band_label='band'):
+        """Build dataframe from a python dictionary
+
+        Parameters
+        ----------
+        data_dict : `dict`
+            Dictionary contaning the data
+        band_label: `str`
+            Name for column contaning filter information
+        """
         index = self._build_index(data_dict[band_label])
         data_dict = {key: data_dict[key] for key in data_dict if key != band_label}
-        self.data = pd.DataFrame(data=data_dict,index=index).sort_index()
+        self.data = pd.DataFrame(data=data_dict, index=index).sort_index()
         return self
 
-    def _from_ensemble(self, data,object_id):
-        """loader function for inputing data from an ensemble"""
+    def _from_ensemble(self, data, object_id):
+        """Loader function for inputing data from an ensemble"""
         self.cols = list(data.columns)
-        self.data=data
-        self.id=object_id
+        self.data = data
+        self.id = object_id
         return self
 
     @property
     def time(self):
         """Time values stored as a Pandas Series"""
         return self.data[self.cols[0]]
-    
+
     @property
     def flux(self):
         """Flux values stored as a Pandas Series"""
         return self.data[self.cols[1]]
-    
+
     @property
     def flux_err(self):
         """Flux error values stored as a Pandas Series"""
         return self.data[self.cols[2]]
-    
+
     @property
     def band(self):
         """Band labels stored as a Pandas Series from Index"""
         return self.data.index.get_level_values('band')
-    
-    def _build_index(self,band):
+
+    def _build_index(self, band):
         """Build pandas multiindex from band array"""
-        #Create a multiindex
-        tuples = zip(band,range(len(band)))
+        # Create a multiindex
+        tuples = zip(band, range(len(band)))
         index = pd.MultiIndex.from_tuples(tuples, names=["band", "index"])
         return index
