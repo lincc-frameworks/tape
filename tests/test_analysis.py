a34976d38862e18a71016e3d1f6433cf6df7a184
Doug Branton <brantd@uw.edu>
2023-02-01 14:46:46 -0800

    sf2 from timeseries test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,6 +1,7 @@
 """Test timeseries analysis functions"""
 
 from lsstseries import timeseries
+import pytest
 
 
 def test_stetsonj():
@@ -21,7 +22,7 @@ def test_stetsonj():
     assert test_ts.stetson_J()["r"] == 0.8
 
 
-def test_sf2():
+def test_sf2_from_timeseries():
     """
     Test of structure function squared function for a known return value
     """
@@ -29,7 +30,6 @@ def test_sf2():
     test_t = [1.11, 2.23, 3.45, 4.01, 5.67, 6.32, 7.88, 8.2]
     test_y = [0.11, 0.23, 0.45, 0.01, 0.67, 0.32, 0.88, 0.2]
     test_yerr = [0.1, 0.023, 0.045, 0.1, 0.067, 0.032, 0.8, 0.02]
-    test_bins = [1, 3, 5, 7]
 
     test_dict = {
         "time": test_t,
@@ -38,8 +38,9 @@ def test_sf2():
         "band": ["r"] * len(test_y),
     }
     ts = timeseries()
+    ts.meta['id'] = 1
     test_ts = ts.from_dict(data_dict=test_dict)
-    res = test_ts.sf2(bins=test_bins)["r"]
+    res = test_ts.sf2(sthresh=100)
 
-    print("test sf2 value is: " + str(res))
-    assert sum(res[1]) == -0.0126072883605957
+    assert res['dt'][0] == pytest.approx(3.705, rel=0.001)
+    assert res['sf2'][0] == pytest.approx(0.005365, rel=0.001)
