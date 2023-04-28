3d1527d536a14bdb6758926edd0612a6f43a0492
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-04-28 16:09:26 -0700

    Cleaning up the input argument validation code.

--- src/lsstseries/analysis/structurefunction2.py
+++ src/lsstseries/analysis/structurefunction2.py
@@ -51,10 +51,7 @@ def calc_sf2(time, flux, err=None, band=None, lc_id=None, sf_method="basic", arg
     # `argument_container`. If any of these arguments are provided with
     # non-default values, we'll use those. Otherwise, we'll look inside
     # `argument_container` and use the values found there.
-    if band is None:
-        band = argument_container.band
-    if band is None:  # Still None after assignment?
-        band = np.full(len(flux), "none")
+    band = _validate_band(band, flux, argument_container)
 
     if lc_id is None:
         lc_id = argument_container.lc_id
@@ -89,36 +86,9 @@ def calc_sf2(time, flux, err=None, band=None, lc_id=None, sf_method="basic", arg
             band_mask = band == b
 
             # Mask on band
-            times = None
+            times = _extract_time(time=time, band_mask=band_mask, argument_container=argument_container)
 
-            # if the user passed in a scalar `None` value, create a numpy array
-            # with a single `None` element. Otherwise assume the user passed an
-            # array of timestamps to be masked with `band_mask`.
-            # Note: some or all timestamps could be `None`.
-            if time is None or argument_container.ignore_timestamps:
-                times = np.array(None)
-            else:
-                times = np.array(time)[band_mask]
-
-            # if all elements in `times` are `None`, we assume equidistant times
-            # between measurements. To do so, we'll create an array of integers
-            # from 0 to N-1 where N is the number of flux values for this band.
-            if np.all(np.equal(times, None)):
-                times = np.arange(sum(band_mask), dtype=int)
-
-            errors = None
-            # assume all errors are 0 if `None` is provided
-            if err is None:
-                errors = np.zeros(sum(band_mask))
-
-            # assume the same error for all measurements if a scalar value is
-            # provided
-            elif np.isscalar(err):
-                errors = np.ones(sum(band_mask)) * err
-
-            # otherwise assume one error value per measurement
-            else:
-                errors = np.array(err)[band_mask]
+            errors = _extract_error(err=err, band_mask=band_mask)
 
             fluxes = np.array(flux)[band_mask]
             lc_ids = np.array(lc_id)[band_mask]
@@ -149,3 +119,90 @@ def calc_sf2(time, flux, err=None, band=None, lc_id=None, sf_method="basic", arg
         {"lc_id": idstack, "band": np.hstack(bands), "dt": np.hstack(dts), "sf2": np.hstack(sf2s)}
     )
     return sf2_df
+
+
+def _validate_band(band, flux, argument_container):
+    # if the input argument `band` is None, check the value of `argument_container.band`
+    if band is None:
+        band = argument_container.band
+
+    # if band is still `None`, create a fake array of bands. We'll use numpy int8
+    # values to use as little memory as possible.
+    if band is None:
+        band = np.full(len(flux), 1, dtype=np.int8)
+
+    if len(band) != len(flux):
+        raise (
+            ValueError,
+            "Value of `band` should be `None` or array with length equal length of `flux` array.",
+        )
+
+    return band
+
+
+def _extract_time(time, band_mask, argument_container):
+    """This will process the input time values, apply a band mask and return a
+    numpy array of timestamps.
+
+    Parameters
+    ----------
+    time : `numpy.ndarray` (N,) or `None`
+        Array of times when measurements were taken. If all array values are
+        `None` or if a scalar `None` is provided, then equidistant time between
+        measurements is assumed.
+    band_mask : `numpy mask` (N,)
+        Array of associated band labels, by default None
+    argument_container : StructureFunctionArgumentContainer, optional
+        Container object for additional configuration options, by default None.
+    """
+
+    times = None
+
+    # if the user passed in a scalar `None` value, or if explicitly ignoring
+    # timestamps using the argument_container flag, create a numpy array
+    # with a single `None` element. Otherwise assume the user passed an
+    # array of timestamps to be masked with `band_mask`.
+    # Note: some or all timestamps could be `None`.
+    if time is None or argument_container.ignore_timestamps:
+        times = np.array(None)
+    else:
+        times = np.array(time)[band_mask]
+
+    # if all elements in `times` are `None`, we assume equidistant times
+    # between measurements. To do so, we'll create an array of integers
+    # from 0 to N-1 where N is the number of flux values for this band.
+    if np.all(np.equal(times, None)):
+        times = np.arange(sum(band_mask), dtype=int)
+
+    return times
+
+
+def _extract_error(err, band_mask):
+    """Process the input err value(s), apply a band_mask, and return a numpy
+    array of error values.
+
+    Parameters
+    ----------
+    err : `numpy.ndarray` (N,), `float`, or `None`
+        Array of associated flux/magnitude errors. If a scalar value is provided
+        we assume that error for all measurements. If `None` is provided, we
+        assume all errors are 0. By default None
+    band_mask : `numpy mask` (N,)
+        Array of associated band labels, by default None
+    """
+
+    errors = None
+    # assume all errors are 0 if `None` is provided
+    if err is None:
+        errors = np.zeros(sum(band_mask))
+
+    # assume the same error for all measurements if a scalar value is
+    # provided
+    elif np.isscalar(err):
+        errors = np.ones(sum(band_mask)) * err
+
+    # otherwise assume one error value per measurement
+    else:
+        errors = np.array(err)[band_mask]
+
+    return errors
--- tests/lsstseries_tests/test_analysis.py
+++ tests/lsstseries_tests/test_analysis.py
@@ -318,3 +318,26 @@ def test_sf2_least_possible_infomation_constant_flux():
 
     assert res["dt"][0] == pytest.approx(4.0, rel=0.001)
     assert res["sf2"][0] == pytest.approx(0.0, rel=0.001)
+
+
+@pytest.mark.skip
+def test_sf2_flux_and_band_different_lengths():
+    """
+    Base test case accessing calc_sf2 directly. Flux and band are different
+    lengths. Expect an exception to be raised.
+    Does not make use of TimeSeries or Ensemble.
+    """
+    lc_id = [1, 1, 1, 1, 1, 1, 1, 1]
+    test_t = [1.11, 2.23, 3.45, 4.01, 5.67, 6.32, 7.88, 8.2]
+    test_y = [0.11, 0.23, 0.45, 0.01, 0.67, 0.32, 0.88, 0.2, 0.3]
+    test_yerr = [0.1, 0.023, 0.045, 0.1, 0.067, 0.032, 0.8, 0.02]
+    test_band = np.array(["r"] * len(test_t))
+
+    with pytest.raises(ValueError) as execinfo:
+        analysis.calc_sf2(
+            time=test_t,
+            flux=test_y,
+            err=test_yerr,
+            band=test_band,
+            lc_id=lc_id,
+        )
