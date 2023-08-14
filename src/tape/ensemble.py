1f62d1244b64fee536d6ce283247d524f3dc86b2
Doug Branton <brantd@uw.edu>
2023-08-14 13:50:10 -0700

    remove TAP query code for now

--- src/tape/ensemble.py
+++ src/tape/ensemble.py
@@ -20,9 +20,8 @@ from .utils import ColumnMapper
 class Ensemble:
     """Ensemble object is a collection of light curve ids"""
 
-    def __init__(self, token=None, client=None, **kwargs):
+    def __init__(self, client=None, **kwargs):
         self.result = None  # holds the latest query
-        self.token = token
 
         self._source = None  # Source Table
         self._object = None  # Object Table
@@ -1248,125 +1247,6 @@ class Ensemble:
         self._object_dirty = False
         return self
 
-    def tap_token(self, token):
-        """Add/update a TAP token to the class, enables querying
-        Read here for information on TAP access:
-        https://data.lsst.cloud/api-aspect
-
-        Parameters
-        ----------
-        token : `str`
-            Token string
-        """
-        self.token = token
-
-    def query_tap(self, query, maxrec=None, debug=False):
-        """Query the TAP service
-
-        Parameters
-        ----------
-        query : `str`
-            Query is an ADQL formatted string
-        maxrec: `int`, optional
-            Max number of results returned
-
-        Returns
-        ----------
-        result: `pd.df`
-            Result of the query, as pandas dataframe
-        """
-        cred = vo.auth.CredentialStore()
-        cred.set_password("x-oauth-basic", self.token)
-        service = vo.dal.TAPService("https://data.lsst.cloud/api/tap", cred.get("ivo://ivoa.net/sso#BasicAA"))
-        time0 = time.time()
-        results = service.search(query, maxrec=maxrec)
-        time1 = time.time()
-        if debug:
-            print(f"Query Time: {time1-time0} (s)")
-        result = results.to_table().to_pandas()
-        self.result = result
-        return result
-
-    def query_ids(
-        self,
-        ids,
-        time_col="midPointTai",
-        flux_col="psFlux",
-        err_col="psFluxErr",
-        add_cols=None,
-        id_field="diaObjectId",
-        catalog="dp02_dc2_catalogs",
-        table="DiaSource",
-        to_mag=True,
-        maxrec=None,
-    ):
-        """Query based on a list of object ids; applicable for DP0.2
-
-        Parameters
-        ----------
-        ids: `int`
-            Ids of object
-        time_col: `str`
-            Column to retrieve and use for time
-        flux_col: `str`
-            Column to retrieve and use for flux (or magnitude or any "signal")
-        err_col: `str`
-            Column to retrieve and use for errors
-        add_cols: `list` of `str`
-            Additional columns to retreive
-        id_field: `str`
-            Which Id is being queried
-        catalog: `str`
-            Source catalog
-        table: `str`
-            Source table
-
-        Returns
-        ----------
-        result: `pd.df`
-            Result of the query, as pandas dataframe
-        """
-        cols = [time_col, flux_col, err_col]
-
-        if to_mag:
-            flux_query, flux_label = self.flux_to_mag([flux_col])
-            flux_col = flux_label[0]
-            if err_col is not None:
-                err_query, err_label = self.flux_to_mag([err_col])
-                err_col = err_label[0]
-
-            query_cols = [time_col] + flux_query + err_query
-            cols = [time_col, flux_col, err_col]
-
-        else:
-            query_cols = cols
-
-        if add_cols is not None:
-            cols = cols + add_cols
-            query_cols = query_cols + add_cols
-
-        idx_cols = ["diaObjectId", "filterName"]
-
-        result = pd.DataFrame(columns=idx_cols + cols)
-        select_cols = ",".join(idx_cols) + "," + ",".join(query_cols)
-        str_ids = [str(obj_id) for obj_id in ids]
-        id_list = "(" + ",".join(str_ids) + ")"
-
-        result = self.query_tap(
-            f"SELECT {select_cols} " f"FROM {catalog}.{table} " f"WHERE {id_field} IN {id_list}",
-            maxrec=maxrec,
-        )
-        index = self._build_index(result["diaObjectId"], result["filterName"])
-        result.index = index
-        result = result[cols].sort_index()
-        self.result = result
-
-        self._time_col = time_col
-        self._flux_col = flux_col
-        self._err_col = err_col
-
-        return result
-
     def to_timeseries(
         self,
         target,
@@ -1427,51 +1307,6 @@ class Ensemble:
             band_label=band_col,
         )
         return ts
-
-    def flux_to_mag(self, cols):
-        """Transforms TAP query from fluxes to magnitudes
-
-         Parameters
-        ----------
-        cols: `list` of `str`
-            List of columns to be queried, containing Flux in the name
-
-        Returns:
-        ----------
-        cols_mag `list` of `str`
-            List of columns to be queried, replaced with magnitudes
-        cols_label 'list' of 'str'
-            List of column labels for the returned quantities
-        """
-
-        cols_mag = []
-        cols_label = []
-        for col in cols:
-            pos_flux = col.find("Flux")
-            if pos_flux == -1:
-                cols_mag.append(col)
-                cols_label.append(col)
-            else:
-                i = pos_flux + len("Flux")
-                pre_var, post_var = col[:pos_flux], col[i:]
-                flux_str = pre_var + "Flux"
-                mag_str = pre_var + "AbMag"
-                if col.find("Err") != -1:
-                    flux_str_err = pre_var + "Flux" + post_var
-                    mag_str_err = pre_var + "AbMag" + post_var
-                    cols_mag.append(
-                        "scisql_nanojanskyToAbMagSigma("
-                        + flux_str
-                        + ","
-                        + flux_str_err
-                        + ") AS "
-                        + mag_str_err
-                    )
-                    cols_label.append(mag_str_err)
-                else:
-                    cols_mag.append("scisql_nanojanskyToAbMag(" + flux_str + ") AS " + mag_str)
-                    cols_label.append(mag_str)
-        return cols_mag, cols_label
 
     def _build_index(self, obj_id, band):
         """Build pandas multiindex from object_ids and bands
