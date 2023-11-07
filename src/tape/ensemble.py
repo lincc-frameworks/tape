48b327526bd7165eaee534af9d742a0bacd3a799
Wilson Beebe <wbeebe@uw.edu>
2023-11-06 16:31:43 -0800

    Provide default labels for result frames.

--- src/tape/ensemble.py
+++ src/tape/ensemble.py
@@ -21,6 +21,8 @@ from .utils import ColumnMapper
 SOURCE_FRAME_LABEL = "source"
 OBJECT_FRAME_LABEL = "object"
 
+DEFAULT_FRAME_LABEL = "result" # A base default label for an Ensemble's result frames.
+
 class Ensemble:
     """Ensemble object is a collection of light curve ids"""
 
@@ -42,6 +44,9 @@ class Ensemble:
         self._object = None  # Object Table
 
         self.frames = {} # Frames managed by this Ensemble, keyed by label
+
+        # A unique ID to allocate new result frame labels.
+        self.default_frame_id = 1
 
         # TODO(wbeebe@uw.edu) Replace self._source and self._object with these 
         self.source = None # Source Table EnsembleFrame
@@ -208,7 +213,7 @@ class Ensemble:
                 f"Unable to select frame: no frame with label" f"'{label}'" f" is in the Ensemble."
                 )
         return self.frames[label]
-
+    
     def frame_info(self, labels=None, verbose=True, memory_usage=True, **kwargs):
         """Wrapper for calling dask.dataframe.DataFrame.info() on frames tracked by the Ensemble.
 
@@ -242,6 +247,18 @@ class Ensemble:
                     )
             print(label, "Frame")
             print(self.frames[label].info(verbose=verbose, memory_usage=memory_usage, **kwargs))
+
+    def _generate_frame_label(self):
+        """ Generates a new unique label for a result frame. """
+        result = DEFAULT_FRAME_LABEL + "_" + str(self.default_frame_id)
+        self.default_frame_id += 1 # increment to guarantee uniqueness
+        while result in self.frames:
+            # If the generated label has been taken by a user, increment again.
+            # In most workflows, we expect the number of frames to be O(100) so it's unlikely for
+            # the performance cost of this method to be high.
+            result = DEFAULT_FRAME_LABEL + "_" + str(self.default_frame_id)
+            self.default_frame_id += 1
+        return result
 
     def insert_sources(
         self,
@@ -983,7 +1000,7 @@ class Ensemble:
         self._source.set_dirty(True)
         return self
 
-    def batch(self, func, *args, meta=None, use_map=True, compute=True, on=None, label=None, **kwargs):
+    def batch(self, func, *args, meta=None, use_map=True, compute=True, on=None, label="", **kwargs):
         """Run a function from tape.TimeSeries on the available ids
 
         Parameters
@@ -1023,7 +1040,9 @@ class Ensemble:
             this is populated automatically.
         label: 'str', optional
             If provided the ensemble will use this label to track the result 
-            dataframe. If `None`, the frame will not be tracked. 
+            dataframe. If not provided, a label of the from "result_{x}" where x
+            is a monotonically increasing integer is generated. If `None`,
+            the result frame will not be tracked. 
         **kwargs:
             Additional optional parameters passed for the selected function
 
@@ -1116,17 +1135,11 @@ class Ensemble:
             )
 
         if label is not None:
-            if isinstance(batch, EnsembleFrame) or isinstance(batch, EnsembleSeries):
-                # Track the result frame under the provided label
-                self.add_frame(batch, label)
-            else:
-                # The result of the batch function was not a frame/series we don't support tracking.
-                # However we opt to provide a warning so that the user won't lose a potentially
-                # expensive computation.
-                warnings.warn(
-                    f"""Warning: Batch successfully computed but failed to track frame under label,
-                      {label}, due to incompatible meta: {meta}""")
-
+            if  label == "":
+                label = self._generate_frame_label()
+                print(f"Using generated label, {label}, for a batch result.")
+            # Track the result frame under the provided label
+            self.add_frame(batch, label)
         if compute:
             return batch.compute()
         else:
--- tests/tape_tests/test_ensemble.py
+++ tests/tape_tests/test_ensemble.py
@@ -1398,7 +1398,6 @@ def test_batch(data_fixture, request, use_map, on):
     """
     Test that ensemble.batch() returns the correct values of the first result
     """
-
     parquet_ensemble = request.getfixturevalue(data_fixture)
     frame_cnt = len(parquet_ensemble.frames)
 
@@ -1432,6 +1431,41 @@ def test_batch(data_fixture, request, use_map, on):
         assert pytest.approx(result.values[1]["g"], 0.001) == 1.2208577
         assert pytest.approx(result.values[1]["r"], 0.001) == -0.49639028
 
+def test_batch_labels(parquet_ensemble):
+    """
+    Test that ensemble.batch() generates unique labels for result frames when none are provided.
+    """
+    # Since no label was provided we generate a label of "result_1"
+    parquet_ensemble.prune(10).batch(np.mean, parquet_ensemble._flux_col)
+    assert "result_1" in parquet_ensemble.frames
+    assert len(parquet_ensemble.select_frame("result_1")) > 0
+
+    # Now give a user-provided custom label.
+    parquet_ensemble.batch(np.mean, parquet_ensemble._flux_col, label="flux_mean")
+    assert "flux_mean" in parquet_ensemble.frames
+    assert len(parquet_ensemble.select_frame("flux_mean")) > 0
+
+    # Since this is the second batch call where a label is *not* provided, we generate label "result_2"
+    parquet_ensemble.batch(np.mean, parquet_ensemble._flux_col)
+    assert "result_2" in parquet_ensemble.frames
+    assert len(parquet_ensemble.select_frame("result_2")) > 0
+
+    # Explicitly provide label "result_3"
+    parquet_ensemble.batch(np.mean, parquet_ensemble._flux_col, label="result_3")
+    assert "result_3" in parquet_ensemble.frames
+    assert len(parquet_ensemble.select_frame("result_3")) > 0
+
+    # Validate that the next generated label is "result_4" since "result_3" is taken.
+    parquet_ensemble.batch(np.mean, parquet_ensemble._flux_col)
+    assert "result_4" in parquet_ensemble.frames
+    assert len(parquet_ensemble.select_frame("result_4")) > 0
+
+    frame_cnt = len(parquet_ensemble.frames)
+
+    # Validate that when the label is None, the result frame isn't tracked by the Ensemble.s
+    result = parquet_ensemble.batch(np.mean, parquet_ensemble._flux_col, label=None)
+    assert frame_cnt == len(parquet_ensemble.frames)
+    assert len(result) > 0
 
 def test_batch_with_custom_func(parquet_ensemble):
     """
