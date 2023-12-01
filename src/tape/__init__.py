6779ba0b4a3cf655791bfcab82cccf75df74d4a1
Wilson Beebe <wbeebe@uw.edu>
2023-11-30 17:45:53 -0800

    Revert "Merge Main into Ensemble Refactor Branch (#304)"
    This reverts commit 5c847e10bd6d4865a1016f8aa621f75129cf8431.

--- src/tape/__init__.py
+++ src/tape/__init__.py
@@ -3,4 +3,3 @@ from .ensemble import *  # noqa
 from .ensemble_frame import * # noqa
 from .timeseries import *  # noqa
 from .ensemble_readers import *  # noqa
-from ._version import __version__  # noqa
--- tests/tape_tests/test_packaging.py
+++
@@ -1,6 +0,0 @@
-import tape
-
-
-def test_version():
-    """Check to see that the version property returns something"""
-    assert tape.__version__ is not None
